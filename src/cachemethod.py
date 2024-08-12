6fa0f19745d28550789e7b30077b55ae702b6219
daniel sonbolian <dsal3389@gmail.com>
2024-08-13 01:13:37 +0300

    added python circular import

--- src/cachemethod.py
+++ src/cachemethod.py
@@ -9,7 +9,8 @@ _RT = TypeVar("_RT")
 
 _CACHE_SEED_ATTR = "__cache_seed__"
 
-_CacheInfo = namedtuple("CacheInfo", ("misses", "hits"))
+_CacheInfo = namedtuple("CacheInfo", ("misses", "hits", "maxsize", "full"))
+_PREV, _NEXT, _KEY, _RESULT = 0, 1, 2, 3
 
 
 class _CacheMiss:
@@ -36,19 +37,20 @@ def _make_seed(used_seeds: set[int]) -> int:
 def _lru_cachemthod_wrapper(
     func: Callable[..., _RT], maxsize: int
 ) -> Callable[..., _RT]:
+    misses = hits = 0
     used_seeds = set()
     lock = Lock()
+    full = False
     cache = {}
 
-    # keeps record of generated seeds and
-    # keys created by this seed in the `cache` dict
-    seed_cache_keys = defaultdict(set)
-
-    misses = hits = 0
+    # root is the root of the circular queue, values
+    # matching the _PREV, _NEXT, _KEY, _RESULT consts
+    root = []
+    root[:] = [root, root, None, None]
 
     @wraps(func)
     def cache_wrapper(__self__, *args, **kwargs) -> _RT:
-        nonlocal misses, hits
+        nonlocal misses, hits, full, root
 
         # get the seed from the instance, if no
         # seed found, generate and set it
@@ -63,47 +65,71 @@ def _lru_cachemthod_wrapper(
         key = _make_cache_key(seed, args, kwargs)
 
         with lock:
-            results = cache.get(key, _CacheMiss)
-            if results is not _CacheMiss:
+            node = cache.get(key, _CacheMiss)
+            if node is not _CacheMiss:
+                prev, next_, key, result = node
+
+                # pop the current node from its position
+                prev[_NEXT] = next_
+                next_[_PREV] = prev
+
+                # push the node after the
+                # last node in the list, making the current node last
+                last = root[_PREV]
+                last[_NEXT] = root[_PREV] = node
+                node[_PREV] = last
+                node[_NEXT] = root
                 hits += 1
-                return results
+                return result
             misses += 1
 
-        seed_cache_keys[seed].add(key)
-        results = func(__self__, *args, **kwargs)
-        cache[key] = results
-        return results
+        result = func(__self__, *args, **kwargs)
+
+        with lock:
+            if key in cache:
+                # based on python stdlib code this can happen
+                # if the same key was added when the lock was released
+                pass
+            elif full:
+                # use the oldest node which is the root
+                # to store the new key and result
+                node = root
+                node[_KEY] = key
+                node[_RESULT] = result
+
+                # mark the next node as current root
+                # marking it also the oldest one
+                root = node[_NEXT]
+                oldkey = root[_KEY]
+
+                # deleting the root, key and result because
+                # the root shouldn't containt data except being
+                # the head of the queue marking the oldest and the newest items
+                root[_KEY] = root[_RESULT] = None
+                del cache[oldkey]
+            else:
+                last = root[_PREV]
+                node = [last, root, key, result]
+                last[_NEXT] = root[_PREV] = cache[key] = node
+                full = len(cache) >= maxsize
+        cache[key] = node
+        return result
 
     def cache_info() -> _CacheInfo:
         """returns the cache info"""
         with lock:
-            return _CacheInfo(misses=misses, hits=hits)
+            return _CacheInfo(misses=misses, hits=hits, maxsize=maxsize, full=full)
 
     def cache_clear() -> None:
         """clears the cache"""
         nonlocal misses, hits
         with lock:
-            seed_cache_keys.clear()
             cache.clear()
             misses = 0
             hits = 0
 
-    def cache_clear_instance(__self__) -> None:
-        seed = getattr(__self__, _CACHE_SEED_ATTR, None)
-        if seed is None:
-            return
-
-        with lock:
-            cache_keys = seed_cache_keys[seed]
-            for cache_key in cache_keys:
-                del cache[cache_key]
-
-            del seed_cache_keys[seed]
-            delattr(__self__, _CACHE_SEED_ATTR)
-
     cache_wrapper.cache_info = cache_info
     cache_wrapper.cache_clear = cache_clear
-    cache_wrapper.cache_clear_instance = cache_clear_instance
     return cache_wrapper
 
 
